d9ecb13bbd94fdd1d8ec65daac52fb0031909a09
irsyad <airsyad@proton.me>
2024-12-15 11:16:33 +0800

    add RadioStation model

---
+++ app/src/main/java/com/airsyad/radiojuicer/data/RadioStation.kt
@@ -0,0 +1,9 @@
+package com.airsyad.radiojuicer.data
+
+data class RadioStation(
+    val stationUiid: String,
+    val name: String,
+    val logo_url: String,
+    val country_name: String,
+    val country_code: String
+)
