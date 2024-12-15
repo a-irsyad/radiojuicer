9cbfbd9cfca528da3f1de4b1fe96c521ae84a457
irsyad <airsyad@proton.me>
2024-12-16 06:20:17 +0800

    add: hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
+    // Hilt, Room
+    alias(libs.plugins.devtools.ksp)
+    // Hilt
+    alias(libs.plugins.dagger.hilt.android)
 }
 
 android {
@@ -56,6 +59,10 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    
 
     testImplementation(libs.junit)
 
