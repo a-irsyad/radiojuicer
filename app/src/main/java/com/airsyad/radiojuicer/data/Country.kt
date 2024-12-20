9da44aa2a8464bf6afcde2ab19df03a445dbb351
irsyad <airsyad@proton.me>
2024-12-20 13:37:06 +0800

    docs: improve documentation for model class `Country`

--- app/src/main/java/com/airsyad/radiojuicer/data/Country.kt
+++ app/src/main/java/com/airsyad/radiojuicer/data/Country.kt
@@ -1,7 +1,12 @@
 package com.airsyad.radiojuicer.data
 
 /**
- * Model representing a country.
+ * Internal model class representing a country.
+ *
+ * This class bridges between layers and components that need to work with data related to a country.
+ * External class or DTO class reprenting a country need to be converted to this class first before
+ * used by other layers or components
+ *
  * @countryCode according to ISO 3166-1
  */
 data class Country(
