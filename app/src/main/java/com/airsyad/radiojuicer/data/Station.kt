c7d2589cfc13a45f68e5d17b2e891487064618ca
irsyad <airsyad@proton.me>
2024-12-20 13:39:53 +0800

    docs: add documentation for model class `Station`

--- app/src/main/java/com/airsyad/radiojuicer/data/Station.kt
+++ app/src/main/java/com/airsyad/radiojuicer/data/Station.kt
@@ -1,5 +1,13 @@
 package com.airsyad.radiojuicer.data
 
+/**
+ * Internal model class representing a Station.
+ *
+ * This class bridges between layers and components that need to work with data related to a Station.
+ * External class or DTO class reprenting a Station need to be converted to this class first before
+ * used by other layers or components
+ *
+ */
 data class Station(
     val stationUiid: String,
     val name: String,
