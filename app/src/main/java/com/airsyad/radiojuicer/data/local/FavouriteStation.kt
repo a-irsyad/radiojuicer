9a9526889d13ba97c2b99318a45b254219bf5d5d
irsyad <airsyad@proton.me>
2024-12-20 13:16:41 +0800

    docs: add documentation for model class `FavouriteStation`

--- app/src/main/java/com/airsyad/radiojuicer/data/local/FavouriteStation.kt
+++ app/src/main/java/com/airsyad/radiojuicer/data/local/FavouriteStation.kt
@@ -3,6 +3,12 @@ package com.airsyad.radiojuicer.data.local
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * Model class representing a favorite radio station entity for the Room database.
+ *
+ * This class is used solely for database-related operations. If other layers or components need access
+ * to a favorite station, it should be converted to the internal model [Station].
+ */
 @Entity(tableName = "favourite_radio_station")
 data class FavouriteStation(
     @PrimaryKey val stationUiid: String,
