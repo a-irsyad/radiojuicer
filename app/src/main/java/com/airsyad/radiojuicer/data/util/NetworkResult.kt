5140d5442f1cad93674ba745bb5f55acf118a1e8
irsyad <airsyad@proton.me>
2024-12-20 13:28:31 +0800

    docs: add documentation for model class `NetworkResult`

--- app/src/main/java/com/airsyad/radiojuicer/data/util/NetworkResult.kt
+++ app/src/main/java/com/airsyad/radiojuicer/data/util/NetworkResult.kt
@@ -1,5 +1,8 @@
 package com.airsyad.radiojuicer.data.util
 
+/**
+ * Used to encapsulate result returned by any network request
+ */
 sealed class NetworkResult<out T> {
     data class Success<T>(val data: T) : NetworkResult<T>()
     data object Loading: NetworkResult<Nothing>()
